b27d26a473fb844b9c8f84c53e72299db417335a
Mauricio Monroy <mauricio.monroy-j@mail.escuelaing.edu.co>
2024-05-18 07:20:13 -0500

    tercer commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // My second change.
+        // jbfaksbfk
         [Key]
         public int CartItemId { get; set; }
 
